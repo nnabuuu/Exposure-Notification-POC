45f8db77da6991fcf57a7b7fe9248007c613b99e
xiaochen.nie <zjsnxc@gmail.com>
2020-05-09 15:29:41 +0800

    Add a 3rd device

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,44 @@
 import {Device} from "./device";
 
-let deviceA = new Device('Device A');
-let deviceB = new Device('Device B');
+function main() {
+    let deviceA = new Device('Device A');
+    let deviceB = new Device('Device B');
+    let deviceC = new Device('Device C');
 
-console.log('====== Generating 14 days records for Device A and Device B');
-let startDate = new Date();
-let date = startDate;
-for(let i = 0; i < 14; i++) {
-    const timestamp = Math.floor(date.getTime() / 1000);
-    deviceA.generateRecord(timestamp);
-    deviceB.generateRecord(timestamp);
-    date.setDate(date.getDate() + 1);
+    console.log('====== Generating 14 days records for Device A, B and C');
+    let startDate = new Date();
+    let date = startDate;
+    for(let i = 0; i < 14; i++) {
+        const timestamp = Math.floor(date.getTime() / 1000);
+        deviceA.generateRecord(timestamp);
+        deviceB.generateRecord(timestamp);
+        deviceC.generateRecord(timestamp);
+        date.setDate(date.getDate() + 1);
+    }
+
+    console.log('====== Device A and Device B exchange data at Day 5 Time frame 16');
+    exchange(deviceA, deviceB, 5, 16);
+
+    console.log('====== Device A and Device B exchange data at Day 5 Time frame 20');
+    exchange(deviceA, deviceC, 5, 20);
+    
+    console.log('Device B\'s TEK upload to cloud then downloaded by A and C');
+    console.log(deviceB.Records[5].TemporaryExposureKey);
+    console.log(deviceB.Records[5].I);
+
+    console.log(`====== Device A Detecting Exposure using TemporaryExposureKey and I:`);
+    let detectedByA = deviceA.detect(deviceB.Records[5].TemporaryExposureKey, deviceB.Records[5].I);
+    console.log(`Device A detect exposure?: ${detectedByA}`);
+
+    console.log(`====== Device C Detecting Exposure using TemporaryExposureKey and I:`);
+    let detectedByC = deviceC.detect(deviceB.Records[5].TemporaryExposureKey, deviceB.Records[5].I);
+    console.log(`Device C detect exposure?: ${detectedByC}`);
+
 }
 
-console.log('====== Device A receiving Device B\'s record at Day 5 Time frame 16');
-let contact = deviceB.Records[5];
-console.log('Rolling Proximity Identifier:');
-console.log(contact.RollingProximityIdentifierRecords[16]);
-console.log('Associated Encrypted Metadata:');
-console.log(contact.AssociatedEncryptedMetadatas[16]);
+function exchange(deviceA: Device, deviceB: Device, day: number, time: number) {
+    deviceA.receiveBroadcastedContact(deviceB.Records[day].RollingProximityIdentifierRecords[time], deviceB.Records[day].AssociatedEncryptedMetadatas[time]);
+    deviceB.receiveBroadcastedContact(deviceA.Records[day].RollingProximityIdentifierRecords[time], deviceA.Records[day].AssociatedEncryptedMetadatas[time]);
+}
 
-deviceA.receiveBroadcastedContact(contact.RollingProximityIdentifierRecords[16], contact.AssociatedEncryptedMetadatas[16]);
-
-console.log(`====== Device A Detecting using TemporaryExposureKey and I:`);
-console.log(contact.TemporaryExposureKey);
-console.log(contact.I);
-const detected = deviceA.detect(contact.TemporaryExposureKey, contact.I);
-console.log(`Detected?: ${detected}`);
-
+main();
